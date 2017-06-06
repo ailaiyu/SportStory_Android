7eae51b2a3b79329afda32573146f585d0129ab6
Zhangligao <zhangligao9509@gmail.com>
2017-06-06 23:40:22 +0800

    update nearby

--- app/src/main/java/cn/sportstory/android/common/tools/TextCheckUtil.java
+++ app/src/main/java/cn/sportstory/android/common/tools/TextCheckUtil.java
@@ -1,5 +1,8 @@
 package cn.sportstory.android.common.tools;
 
+import android.text.TextUtils;
+import android.widget.TextView;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,22 +12,16 @@ import java.util.regex.Pattern;
 
 public class TextCheckUtil {
 
-    public static final String REGEX_EMAIL =
+    private static final String REGEX_EMAIL =
             "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 
-    public static final String REGEX_MOBILE =
+    private static final String REGEX_MOBILE =
             "^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
     public static boolean IsPhone(String phone){
-        if (phone == null){
-            return false;
-        }
-        return Pattern.matches(REGEX_MOBILE, phone);
+        return !TextUtils.isEmpty(phone) && Pattern.matches(REGEX_MOBILE, phone);
     }
 
     public static boolean IsEmail(String email){
-
-        if (email == null)
-            return false;
-        return Pattern.matches(REGEX_EMAIL, email);
+        return !TextUtils.isEmpty(email) && Pattern.matches(REGEX_EMAIL, email);
     }
 }
---
+++ app/src/main/java/cn/sportstory/android/settings/view/SettingsActivity.java
@@ -0,0 +1,11 @@
+package cn.sportstory.android.settings.view;
+
+import cn.sportstory.android.BaseActivity;
+
+/**
+ * Created by aaron on 2017/6/6.
+ */
+
+public class SettingsActivity extends BaseActivity {
+
+}

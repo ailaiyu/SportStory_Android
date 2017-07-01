5390230dd8e01a332caa019b9f7a455fbf1a6588
Zhangligao <zhangligao9509@gmail.com>
2017-07-01 14:35:30 +0800

    update register

---
+++ app/src/main/java/cn/sportstory/android/common/tools/QiNiuUploadCallback.java
@@ -0,0 +1,8 @@
+package cn.sportstory.android.common.tools;
+
+/**
+ * Created by aaron on 2017/7/1.
+ */
+
+public class QiNiuUploadCallback {
+}
---
+++ app/src/main/java/cn/sportstory/android/common/view/ProcessView.java
@@ -0,0 +1,8 @@
+package cn.sportstory.android.common.view;
+
+/**
+ * Created by aaron on 2017/7/1.
+ */
+
+public class ProcessView {
+}
